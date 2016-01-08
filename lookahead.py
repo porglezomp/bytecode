ea5d6647d737bea9e63f05c490499fcf39643f76
Caleb Jones <self@calebjones.net>
2016-01-08 16:34:37 -0500

    Change Lookahead to handle None in generators
    Add Lookahead.done field to track completion instead of setting _peek to None
    Return None as lookahead if the peek() is off the end of the wrapped generator

--- lookahead.py
+++ lookahead.py
@@ -1,14 +1,16 @@
 class Lookahead:
     """
     Wrap a generator to include a single object of lookahead.
-    Doesn't correctly handle generators which yield `None`.
+    Will return None if you peek() at a completed generator
     """
     def __init__(self, gen):
         self.gen = gen
+        self.done = False
         try:
             self._peek = self.gen.next()
         except StopIteration:
             self._peek = None
+            self.done = True
 
     def peek(self):
         return self._peek
@@ -18,13 +20,14 @@ class Lookahead:
 
     def next(self):
         value = self._peek
-        if value is None:
+        if self.done:
             raise StopIteration
 
         try:
             self._peek = self.gen.next()
         except StopIteration:
             self._peek = None
+            self.done = True
 
         return value
 

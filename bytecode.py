58e36e4e1292ce42179ac91515c36ba15acda677
Caleb Jones <self@calebjones.net>
2016-01-08 11:49:01 -0500

    Add Code classes
    Add Code subclasses for PushNum, LoadLocal, StoreLocal, and MathOp

--- bytecode.py
+++ bytecode.py
@@ -176,6 +176,20 @@ def parse_expr(tokens):
     return expr
 
 
+class Code:
+    def __init__(self, value):
+        self.value = value
+
+    def __repr__(self):
+        return "{}({!r})".format(self.__class__.__name__, self.value)
+
+
+class PushNum (Code): pass
+class LoadLocal (Code): pass
+class StoreLocal (Code): pass
+class MathOp (Code): pass
+
+
 def compile(ast):
     pass
 

17ba5787ee07b35ed872d59231b7957a87e64db5
Caleb Jones <self@calebjones.net>
2016-01-08 12:49:32 -0500

    Add lookahead to the lexer
    Implement Lookahead which wraps generators to add a single object lookahead
    Write the lookahead decorator to wrap generator functions
    Apply @lookahead to tokenize()

--- bytecode.py
+++ bytecode.py
@@ -1,6 +1,50 @@
 import re
 
 
+class Lookahead:
+    """
+    Wrap a generator to include a single object of lookahead.
+    Doesn't correctly handle generators which yield `None`.
+    """
+    def __init__(self, gen):
+        self.gen = gen
+        try:
+            self._peek = self.gen.next()
+        except StopIteration:
+            self._peek = None
+
+    def peek(self):
+        return self._peek
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        value = self._peek
+        if value is None:
+            raise StopIteration
+
+        try:
+            self._peek = self.gen.next()
+        except StopIteration:
+            self._peek = None
+
+        return value
+
+    def skip(self):
+        try:
+            self.next()
+        except:
+            pass
+        return self
+
+
+def lookahead(fn):
+    def wrapped(*args):
+        return Lookahead(fn(*args))
+    return wrapped
+
+
 class Token:
     def __init__(self, value):
         self.value = value
@@ -31,6 +75,7 @@ handlers = [
 ]
 
 
+@lookahead
 def tokenize(string):
     while True:
         string = string.lstrip()

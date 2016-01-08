40656673b0d3046b27ddf46d6ef6eb3edcca1d19
Caleb Jones <self@calebjones.net>
2016-01-08 12:41:49 -0500

    Add a Token class
    Implement equality, printing, and an isa() method
    isa() is sugar for isinstance()
    
    Add Token subclasses for Ident, Num, Char, and Op

--- bytecode.py
+++ bytecode.py
@@ -1,3 +1,26 @@
+class Token:
+    def __init__(self, value):
+        self.value = value
+
+    def __eq__(self, other):
+        if isinstance(other, Token):
+            return self.value == other.value
+        else:
+            return self.value == other
+
+    def __repr__(self):
+        return "{}({})".format(self.__class__.__name__, repr(self.value))
+
+    def isa(self, ty):
+        return isinstance(self, ty)
+
+
+class Ident (Token): pass
+class Num (Token): pass
+class Char (Token): pass
+class Op (Token): pass
+
+
 def tokenize(string):
     pass
 
@@ -12,4 +35,3 @@ def compile(ast):
 
 def interp(bytecode):
     pass
-

836655ec2c4c7644be70622c523111873b290ed9
Caleb Jones <self@calebjones.net>
2016-01-09 00:46:48 -0500

    Add prettyprinting for the bytecode representation

--- codegen.py
+++ codegen.py
@@ -1,3 +1,7 @@
+def pad(string, width):
+    return string + " " * (width - len(string))
+
+
 class Code:
     def __init__(self, value):
         self.value = value
@@ -6,10 +10,32 @@ class Code:
         return "{}({!r})".format(self.__class__.__name__, self.value)
 
 
-class PushNum (Code): pass
-class LoadLocal (Code): pass
-class StoreLocal (Code): pass
-class MathOp (Code): pass
+class PushNum (Code):
+    def __str__(self):
+        return pad("PUSH_NUM", 15) + str(self.value)
+
+
+class LoadLocal (Code):
+    def __str__(self):
+        return pad("LOAD_LOCAL", 15) + str(self.value)
+
+
+class StoreLocal (Code):
+    def __str__(self):
+        return pad("STORE_LOCAL", 15) + str(self.value)
+
+
+class MathOp (Code):
+    op_names = {
+        '+': 'ADD',
+        '-': 'SUB',
+        '*': 'MUL',
+        '/': 'DIV',
+        '^': 'EXP',
+    }
+
+    def __str__(self):
+        return 'OP_' + MathOp.op_names[self.value]
 
 
 def codegen(ast):
